601cbda58ebeb9760da3500c6952753627924517
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-04 14:33:27 +0100

    Added ReturnZero_WhenAdding_GivenEmptyString test

--- StringCalculatorKata.Source/Class1.cs
+++ StringCalculatorKata.Source/Class1.cs
@@ -8,5 +8,6 @@ namespace StringCalculatorKata.Source
 {
     public class Class1
     {
+        
     }
 }
---
+++ StringCalculatorKata.Tests/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace StringCalculatorKata.Tests
+{
+    using System;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringCalculatorServiceShould
+    {
+        [Test]
+        public void ReturnZero_WhenAdding_GivenEmptyString()
+        {
+            var stringCalculatorService = new StringCalculatorService();
+
+            var result = stringCalculatorService.Add("");
+
+            result.Should().Be(0);
+        }
+
+    }
+
+    public class StringCalculatorService
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
